6dfc3c502173a384f371d07f0bb082637f06ae1c
HeyYukio <yukiomrt@gmail.com>
2023-07-01 23:59:13 -0300

    new place form added

--- src/places/pages/NewPlace.tsx
+++ src/places/pages/NewPlace.tsx
@@ -1,5 +1,13 @@
-function NewPlace(){
-    return <h2>NewPlace Works!</h2>
+import Input from "../../shared/components/FormElements/Input";
+
+import './NewPlace.css'
+
+function NewPlace() {
+  return(
+    <form className="place-form">
+        <Input element='input' type="text" label="Title"/>
+    </form>
+  );
 }
 
-export default NewPlace
+export default NewPlace;
